75512309c6874daff59b7c48ffb154ffd4a1a2ed
emavi98 <emanuelvm04@gmail.com>
2022-09-12 21:28:35 +0200

    eslint and prettier fast config for react projects

---
+++ commands.js
@@ -0,0 +1,32 @@
+
+//Comand List for install eslint and prettier in a project
+
+/*
+if yarn project then:
+Installing eslint and config:
+yarn add eslint 
+npm init @eslint/config // estandar config
+
+install prettier
+yarn add prettier
+
+
+	"devDependencies": {
+		"@types/react": "^18.0.17",
+		"@types/react-dom": "^18.0.6",
+		"@vitejs/plugin-react": "^2.0.1",
+		"autoprefixer": "^10.4.8",
+		"eslint-config-prettier": "^8.5.0",
+		"eslint-config-standard": "^17.0.0",
+		"eslint-plugin-import": "^2.25.2",
+		"eslint-plugin-n": "^15.0.0",
+		"eslint-plugin-promise": "^6.0.0",
+		"eslint-plugin-react": "^7.31.8",
+		"postcss": "^8.4.16",
+		"prettier": "^2.7.1",
+		"tailwindcss": "^3.1.8",
+		"vite": "^3.0.7"
+	}
+
+
+*/
